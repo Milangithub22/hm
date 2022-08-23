18d0466c358f5b5116d9301141ba23498bfac613
Milangithub22 <95203140+Milangithub22@users.noreply.github.com>
2022-08-23 22:39:53 +0200

    s

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,82 +1,37 @@
 import React from "react";
+import { nanoid } from "nanoid";
 
 export default function Portfolio() {
   const dataPortfolio = [
     {
       title: "quizzical-app",
-
-      image: "canadian-wanderlust.jpg",
+      key: nanoid(),
+      image: "NEuip.jpg",
       url: "https://quizzical-app01.netlify.app/",
     },
     {
       title: "tenzies-game",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://tenzies-game01.netlify.app/",
-    },
-    {
-      title: "quizzical-app",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://quizzical-app01.netlify.app/",
-    },
-    {
-      title: "tenzies-game",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://tenzies-game01.netlify.app/",
-    },
-    {
-      title: "quizzical-app",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://quizzical-app01.netlify.app/",
-    },
-    {
-      title: "tenzies-game",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://tenzies-game01.netlify.app/",
-    },
-    {
-      title: "quizzical-app",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://quizzical-app01.netlify.app/",
-    },
-    {
-      title: "tenzies-game",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://tenzies-game01.netlify.app/",
-    },
-    {
-      title: "quizzical-app",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://quizzical-app01.netlify.app/",
-    },
-    {
-      title: "tenzies-game",
-
-      image: "canadian-wanderlust.jpg",
-      url: "https://tenzies-game01.netlify.app/",
+      key: nanoid(),
+      image: "NEuip.jpg",
+      url: "https://tenzies-game01.netlify.app",
     },
   ];
 
   const Items = dataPortfolio.map((data) => {
     return (
-      <div key={data.title}>
+      <div key={data.key}>
         <div>
           <a href={data.url} title={data.title}>
-            <img alt={data.title} src={`../portfolio/${data.image}`} />
+            <img
+              alt={data.title}
+              style={{ height: 100, width: 100 }}
+              src={require(`./Images/${data.image}`)}
+            />
+
             <div>
               <div>
                 <h5>{data.title}</h5>
               </div>
-            </div>
-            <div className="link-icon">
-              <i className="fa fa-link"></i>
             </div>
           </a>
         </div>
@@ -89,7 +44,6 @@ export default function Portfolio() {
       <div className="portfolio_container">
         <div>
           <h1 className="portfolio_title">Here are a few of mine projects.</h1>
-
           <div className="portfolio_items">{Items}</div>
         </div>
       </div>
